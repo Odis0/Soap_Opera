be59a899f7375f88e65e61640f39112f1b2d5299
Spencer Osburn <sjosburn@gmail.com>
2021-10-08 22:53:48 -0700

    I can get an entity to move from one place to another! But the target destination and the target entity are a mix of objects and enums. So that needs to be standardized.

--- main.py
+++ main.py
@@ -19,19 +19,57 @@ class RoomID():
     Landing = 15
     Studio = 16
 
+class EntityID():
+    Ethan = 0
 
 
 class Room:
-    def __init__(self, roomName, xAxis, yAxis, zAxis, connectionList):
+    def __init__(self, roomName, xAxis, yAxis, zAxis, connectionList, roomInventory = []):
         self.__roomName = roomName
         self.__roomCoordinates = (xAxis, yAxis, zAxis)
         self.__connectionList = connectionList
+        self.__roomInventory = roomInventory
 
     def GetRoomConnections(self):
         return self.__connectionList
 
+    def GetRoomInventory(self):
+        return self.__roomInventory
+
+    def SetRoomInventory(self, targetEntity):
+        self.__roomInventory.append(targetEntity)
+
+    def RemoveRoomInventory(self,targetEntity):
+        self.__roomInventory.remove(targetEntity)
+
+    def GetRoomName(self):
+        return self.__roomName
 
 
+class Entity:
+    def __init__(self, entityName, entityRoom):
+        self.__entityName = entityName
+        self.__entityRoom = entityRoom
+
+    def GetEntityRoom(self):
+        return self.__entityRoom
+
+    def GetEntityName(self):
+        return self.__entityName
+
+
+def EntityMove(targetEntity,targetDestination):
+    roomList[targetDestination].SetRoomInventory(targetEntity)
+    roomList[targetEntity.GetEntityRoom()].RemoveRoomInventory(targetEntity)
+    print(f"{targetEntity.GetEntityName()} moved from {roomList[targetEntity.GetEntityRoom()].GetRoomName()} to {roomList[targetDestination].GetRoomName()}.")
+
+
+
+
+
+
+entityList = [Entity("Ethan",RoomID.Kitchen)
+]
 
 
 roomList = [Room("Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
@@ -39,7 +77,7 @@ roomList = [Room("Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
             Room("Office", 0, 1, 1, connectionList=[RoomID.Entry_Hall]),
             Room("Entry Hall", 1, 1, 1, connectionList=[RoomID.Office,RoomID.Garage,RoomID.Kitchen]),
             Room("Garage", 2, 1, 1, connectionList=[RoomID.Entry_Hall]),
-            Room("Kitchen", 1, 2, 1, connectionList=[RoomID.Entry_Hall,RoomID.Living_Room,RoomID.Basement,RoomID.Landing]),
+            Room("Kitchen", 1, 2, 1, connectionList=[RoomID.Entry_Hall,RoomID.Living_Room,RoomID.Basement,RoomID.Landing],roomInventory=[entityList[EntityID.Ethan]]),
             Room("Daughter's Room", 0,3,1,connectionList=[RoomID.Living_Room]),
             Room("Living Room",1,3,1, connectionList=[RoomID.Daughter_Room,RoomID.Kitchen,RoomID.Back_Patio,RoomID.Down_Bathroom]),
             Room("Downstairs Bathroom",2,3,1, connectionList=[RoomID.Living_Room]),
@@ -54,3 +92,7 @@ roomList = [Room("Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
             ]
 
 print(roomList[RoomID.Kitchen].GetRoomConnections())
+print(roomList[RoomID.Entry_Hall].GetRoomInventory())
+print(roomList[RoomID.Kitchen].GetRoomInventory())
+EntityMove(entityList[EntityID.Ethan],RoomID.Entry_Hall)
+print(roomList[RoomID.Entry_Hall].GetRoomInventory())
