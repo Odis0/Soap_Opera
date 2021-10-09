c2afa0ede371ef306ca473dbd2ad5b6b4eb4d21f
Spencer Osburn <sjosburn@gmail.com>
2021-10-08 22:25:37 -0700

    Now I've got them all connected hahahaha. Now I've got to get somebody in there that can move around.

--- main.py
+++ main.py
@@ -22,26 +22,35 @@ class RoomID():
 
 
 class Room:
-    def __init__(self, roomName, xAxis, yAxis, zAxis):
+    def __init__(self, roomName, xAxis, yAxis, zAxis, connectionList):
         self.__roomName = roomName
         self.__roomCoordinates = (xAxis, yAxis, zAxis)
+        self.__connectionList = connectionList
+
+    def GetRoomConnections(self):
+        return self.__connectionList
 
 
-roomList = [Room("Basement", 1 , 2, 0),
-            Room("Front Yard", 0 , 1, 1),
-            Room("Office", 0, 1, 1),
-            Room("Entry Hall", 1, 1, 1),
-            Room("Garage", 2, 1, 1),
-            Room("Kitchen", 1, 2, 1),
-            Room("Daughter's Room", 0,3,1),
-            Room("Living Room",1,3,1),
-            Room("Downstairs Bathroom",2,3,1),
-            Room("Backyard Patio",1,4,1),
-            Room("Pool",2,4,1),
-            Room("Walk-in Closet",1,0,2),
-            Room("Master Bathroom",0,1,2),
-            Room("Master Bedroom",1,1,2),
-            Room("Son's Room",0,2,2),
-            Room("Upper Landing", 1,2,2),
-            Room("Art Studio",1,3,2)
+
+
+
+roomList = [Room("Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
+            Room("Front Yard", 0 , 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room("Office", 0, 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room("Entry Hall", 1, 1, 1, connectionList=[RoomID.Office,RoomID.Garage,RoomID.Kitchen]),
+            Room("Garage", 2, 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room("Kitchen", 1, 2, 1, connectionList=[RoomID.Entry_Hall,RoomID.Living_Room,RoomID.Basement,RoomID.Landing]),
+            Room("Daughter's Room", 0,3,1,connectionList=[RoomID.Living_Room]),
+            Room("Living Room",1,3,1, connectionList=[RoomID.Daughter_Room,RoomID.Kitchen,RoomID.Back_Patio,RoomID.Down_Bathroom]),
+            Room("Downstairs Bathroom",2,3,1, connectionList=[RoomID.Living_Room]),
+            Room("Backyard Patio",1,4,1, connectionList=[RoomID.Living_Room,RoomID.Pool]),
+            Room("Pool",2,4,1, connectionList=[RoomID.Back_Patio]),
+            Room("Walk-in Closet",1,0,2, connectionList=[RoomID.Mast_Bedroom]),
+            Room("Master Bathroom",0,1,2, connectionList=[RoomID.Mast_Bedroom]),
+            Room("Master Bedroom",1,1,2, connectionList=[RoomID.Mast_Bathroom,RoomID.Closet,RoomID.Landing]),
+            Room("Son's Room",0,2,2, connectionList=[RoomID.Landing]),
+            Room("Upper Landing", 1,2,2, connectionList=[RoomID.Kitchen,RoomID.Mast_Bedroom,RoomID.Son_Room,RoomID.Studio]),
+            Room("Art Studio",1,3,2, connectionList=[RoomID.Landing])
             ]
+
+print(roomList[RoomID.Kitchen].GetRoomConnections())
