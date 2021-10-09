684205b9343549d9ed6a17569ffd30fc7b4963fc
Spencer Osburn <sjosburn@gmail.com>
2021-10-09 16:57:31 -0700

    Made some helper functions to make it easier to get the room of Ethan and the name of his room. Also now changes where the entity knows where it is.

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ class EntityID():
 
 
 class Room:
-    def __init__(self, roomName, xAxis, yAxis, zAxis, connectionList, roomInventory = []):
+    def __init__(self, roomID, roomName, xAxis, yAxis, zAxis, connectionList, roomInventory = []):
+        self.__roomID = roomID
         self.__roomName = roomName
         self.__roomCoordinates = (xAxis, yAxis, zAxis)
         self.__connectionList = connectionList
@@ -45,6 +46,9 @@ class Room:
     def GetRoomName(self):
         return self.__roomName
 
+    def GetRoomID(self):
+        return self.__roomID
+
 
 class Entity:
     def __init__(self, entityName, entityRoom):
@@ -57,13 +61,22 @@ class Entity:
     def GetEntityName(self):
         return self.__entityName
 
+    def SetEntityRoom(self, targetDestination):
+        self.__entityRoom = targetDestination
+
 
 def EntityMove(targetEntity,targetDestination):   #Pass in the objects themselves.
+    startRoom = targetEntity.GetEntityRoom()
     targetDestination.AddRoomInventory(targetEntity)
-    roomList[targetEntity.GetEntityRoom()].RemoveRoomInventory(targetEntity)
-    print(f"{targetEntity.GetEntityName()} moved from {roomList[targetEntity.GetEntityRoom()].GetRoomName()} to {targetDestination.GetRoomName()}.")
+    roomList[startRoom].RemoveRoomInventory(targetEntity)
+    targetEntity.SetEntityRoom(targetDestination.GetRoomID())
+    print(f"{targetEntity.GetEntityName()} moved from {roomList[startRoom].GetRoomName()} to {targetDestination.GetRoomName()}.")
 
+def GetEntityRoom(targetEntity):
+    return roomList[targetEntity.GetEntityRoom()]
 
+def GetEntityRoomName(targetEntity):
+    return GetEntityRoom(targetEntity).GetRoomName()
 
 
 
@@ -72,23 +85,23 @@ entityList = [Entity("Ethan",RoomID.Kitchen)
 ]
 
 
-roomList = [Room("Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
-            Room("Front Yard", 0 , 1, 1, connectionList=[RoomID.Entry_Hall]),
-            Room("Office", 0, 1, 1, connectionList=[RoomID.Entry_Hall]),
-            Room("Entry Hall", 1, 1, 1, connectionList=[RoomID.Office,RoomID.Garage,RoomID.Kitchen]),
-            Room("Garage", 2, 1, 1, connectionList=[RoomID.Entry_Hall]),
-            Room("Kitchen", 1, 2, 1, connectionList=[RoomID.Entry_Hall,RoomID.Living_Room,RoomID.Basement,RoomID.Landing],roomInventory=[entityList[EntityID.Ethan]]),
-            Room("Daughter's Room", 0,3,1,connectionList=[RoomID.Living_Room]),
-            Room("Living Room",1,3,1, connectionList=[RoomID.Daughter_Room,RoomID.Kitchen,RoomID.Back_Patio,RoomID.Down_Bathroom]),
-            Room("Downstairs Bathroom",2,3,1, connectionList=[RoomID.Living_Room]),
-            Room("Backyard Patio",1,4,1, connectionList=[RoomID.Living_Room,RoomID.Pool]),
-            Room("Pool",2,4,1, connectionList=[RoomID.Back_Patio]),
-            Room("Walk-in Closet",1,0,2, connectionList=[RoomID.Mast_Bedroom]),
-            Room("Master Bathroom",0,1,2, connectionList=[RoomID.Mast_Bedroom]),
-            Room("Master Bedroom",1,1,2, connectionList=[RoomID.Mast_Bathroom,RoomID.Closet,RoomID.Landing]),
-            Room("Son's Room",0,2,2, connectionList=[RoomID.Landing]),
-            Room("Upper Landing", 1,2,2, connectionList=[RoomID.Kitchen,RoomID.Mast_Bedroom,RoomID.Son_Room,RoomID.Studio]),
-            Room("Art Studio",1,3,2, connectionList=[RoomID.Landing])
+roomList = [Room(0, "Basement", 1 , 2, 0, connectionList=[RoomID.Kitchen]),
+            Room(1, "Front Yard", 0 , 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room(2, "Office", 0, 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room(3, "Entry Hall", 1, 1, 1, connectionList=[RoomID.Office,RoomID.Garage,RoomID.Kitchen]),
+            Room(4, "Garage", 2, 1, 1, connectionList=[RoomID.Entry_Hall]),
+            Room(5, "Kitchen", 1, 2, 1, connectionList=[RoomID.Entry_Hall,RoomID.Living_Room,RoomID.Basement,RoomID.Landing],roomInventory=[entityList[EntityID.Ethan]]),
+            Room(6, "Daughter's Room", 0,3,1,connectionList=[RoomID.Living_Room]),
+            Room(7, "Living Room",1,3,1, connectionList=[RoomID.Daughter_Room,RoomID.Kitchen,RoomID.Back_Patio,RoomID.Down_Bathroom]),
+            Room(8, "Downstairs Bathroom",2,3,1, connectionList=[RoomID.Living_Room]),
+            Room(9, "Backyard Patio",1,4,1, connectionList=[RoomID.Living_Room,RoomID.Pool]),
+            Room(10, "Pool",2,4,1, connectionList=[RoomID.Back_Patio]),
+            Room(11, "Walk-in Closet",1,0,2, connectionList=[RoomID.Mast_Bedroom]),
+            Room(12, "Master Bathroom",0,1,2, connectionList=[RoomID.Mast_Bedroom]),
+            Room(13, "Master Bedroom",1,1,2, connectionList=[RoomID.Mast_Bathroom,RoomID.Closet,RoomID.Landing]),
+            Room(14, "Son's Room",0,2,2, connectionList=[RoomID.Landing]),
+            Room(15, "Upper Landing", 1,2,2, connectionList=[RoomID.Kitchen,RoomID.Mast_Bedroom,RoomID.Son_Room,RoomID.Studio]),
+            Room(16, "Art Studio",1,3,2, connectionList=[RoomID.Landing])
             ]
 
 print(roomList[RoomID.Kitchen].GetRoomConnections())
@@ -96,3 +109,4 @@ print(roomList[RoomID.Entry_Hall].GetRoomInventory())
 print(roomList[RoomID.Kitchen].GetRoomInventory())
 EntityMove(entityList[EntityID.Ethan],roomList[RoomID.Entry_Hall])
 print(roomList[RoomID.Entry_Hall].GetRoomInventory())
+print(f"Ethan is now in {GetEntityRoomName(entityList[EntityID.Ethan])}")
